a9ab2f6cee5265167a6e98bda7b7b17ca8883b04
Yutong Wang <aeo9539@gmail.com>
2024-07-23 23:19:04 -0400

    Part 2: exercises 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
@@ -6,6 +7,12 @@ function App() {
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [nameFilter, setNameFilter] = useState('');
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/persons").then(response => {
+      setPersons(response.data);
+    })
+  }, []);
 
   const addPerson = (event) => {
     setNewName(event.target.value);
